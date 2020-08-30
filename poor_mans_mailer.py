42e8a0569282adac2631e8a545bc3bf9bea1a870
Chad Phillips <chad@apartmentlines.com>
2020-08-29 18:16:04 -0700

    flesh out rest of basic architecture

--- poor_mans_mailer.py
+++ poor_mans_mailer.py
@@ -1,24 +1,29 @@
+import logging
 import smtplib
 from email.mime.text import MIMEText
 
+POOR_MANS_NAGIOS_GIT_URL = "https://github.com/stirlab/poor-mans-nagios"
+
 class PoorMansMailer(object):
-    sender = 'admin@example.com'
-    receiver = 'info@example.com'
+    def __init__(self, email_from, logger):
+        self.email_from = email_from
+        self.logger = logger
+        self.debug = self.logger.level == logging.DEBUG
 
-    msg = MIMEText('This is test mail')
+    def send_problem_alert(self, alert_emails, host, check_command):
+        subject = '[PROBLEM] %s failed on %s' % (check_command, host)
+        self.send(subject, alert_emails)
 
+    def send_recovery_alert(self, alert_emails, host, check_command):
+        subject = '[RECOVERY] %s succeeded on %s' % (check_command, host)
+        self.send(subject, alert_emails)
 
-  problem-subject: '[PROBLEM] %command failed on %host'
-  recovery-subject: '[RECOVERY] %command succeeded on %host'
-    msg['Subject'] = 'Test mail'
-    msg['From'] = 'admin@example.com'
-    msg['To'] = 'info@example.com'
-
-    user = 'username'
-    password = 'passoword'
-
-    with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-
-        server.login(user, password)
-        server.sendmail(sender, receiver, msg.as_string())
-        print("mail successfully sent")
+    def send(self, subject, recipients):
+        with smtplib.SMTP("localhost") as server:
+            if self.debug:
+                server.set_debuglevel(1)
+            msg = MIMEText("Sent from poor-mans-nagios: %s" % POOR_MANS_NAGIOS_GIT_URL)
+            msg['Subject'] = subject
+            msg['From'] = email_from
+            msg['To'] = ", ".join(recipients)
+            server.sendmail(self.email_from, recipients, msg.as_string())
--- poor_mans_nagios.py
+++ poor_mans_nagios.py
@@ -12,13 +12,16 @@ pp = pprint.PrettyPrinter(indent=4)
 
 DEFAULT_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 DEFAULT_CONFIG_FILE = "%s/config.yaml" % DEFAULT_SCRIPT_DIR
+DEFAULT_CHECK_INTERVAL = 5
+DEFAULT_RETRY_INTERVAL = 1
+DEFAULT_FAILURE_THRESHOLD = 5
+DEFAULT_NRPE_BINARY = "/usr/lib/nagios/plugins/check_nrpe"
 
 class PoorMansNagios(object):
 
     def __init__(self, config_file=None, args={}):
         self.config = self.parse_config(config_file or DEFAULT_CONFIG_FILE)
         self.config.update(args)
-        self.defaults = self.config['defaults']
         self.quiet = 'quiet' in self.config and self.config['quiet']
         self.debug = 'debug' in self.config and self.config['debug']
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -29,6 +32,9 @@ class PoorMansNagios(object):
             self.enable_debug()
             self.logger.debug('Config:')
             pp.pprint(self.config)
+        self.build_configuration()
+        self.mailer = PoorMansMailer(self.email_from, self.logger)
+        self.reset_on_check_ok()
 
     def default_loglevel(self):
         self.logger.setLevel(self.log_level)
@@ -47,29 +53,124 @@ class PoorMansNagios(object):
                 raise RuntimeError("Could not load config file %s: %s" % (config_file, err))
             return config
 
-    def execute_check(self):
-        data = self.get_cluster_master()
-        if data:
-            needs_update, hostname, port, slave_hosts = self.master_needs_update(data)
-            if needs_update:
-                self.logger.info("Master %s:%d exists, needs update, with slaves: %s" % (hostname, port, slave_hosts))
-            return needs_update, hostname, port, slave_hosts
-        return result
+    def build_configuration(self):
+        pmn_config = self.config['poor-mans-nagios']
+        nrpe_config = self.config['nrpe']
+        try:
+            self.nrpe_binary = pmn_config['nrpe-binary']
+        except KeyError:
+            self.nrpe_binary = DEFAULT_NRPE_BINARY
+        try:
+            self.check_interval = pmn_config['check-interval']
+        except KeyError:
+            self.check_interval = DEFAULT_CHECK_INTERVAL
+        try:
+            self.retry_interval = pmn_config['retry-interval']
+        except KeyError:
+            self.retry_interval = DEFAULT_RETRY_INTERVAL
+        try:
+            self.failure_threshold = pmn_config['failure-threshold']
+        except KeyError:
+            self.failure_threshold = DEFAULT_FAILURE_THRESHOLD
+        try:
+            self.alert_on_recovery = pmn_config['alert-on-recovery']
+        except KeyError:
+            self.alert_on_recovery = True
+        try:
+            self.alert_emails = pmn_config['alert-emails']
+        except KeyError:
+            self.alert_emails = []
+        self.email_from = pmn_config['email-from']
+        self.checked_host = nrpe_config['host']
+        self.check_command = nrpe_config['command']
 
-    def next_action(self, result):
-        sleep_seconds = self.auto_update_interval_seconds
-        if needs_update and self.checks >= self.auto_update_check_threshold:
-            self.logger.info("Master needs update and passed check_threshold %d, setting writable" % self.auto_update_check_threshold)
-            self.set_instance_writeable(hostname, port)
-            sleep_seconds *= self.auto_update_check_threshold
-        self.logger.debug("Waiting %d seconds before next check" % sleep_seconds)
-        time.sleep(sleep_seconds)
+    def set_sleep_seconds(self, minutes):
+        self.sleep_seconds = minutes * 60
+
+    def reset_on_check_ok(self):
+        self.logger.info("Resetting tracking on recovery")
+        self.fail_count = 0
+        self.alert_sent = False
+        self.set_sleep_seconds(self.check_interval)
+
+    def check_failure_threshold(self):
+        return self.fail_count >= self.failure_threshold:
+
+    def handle_failure(self):
+        self.fail_count += 1
+        if self.check_failure_threshold():
+            self.send_problem_alert() if not self.alert_sent
+
+    def handle_recovery(self):
+        if self.alert_sent:
+            self.logger.info("Service recovered")
+            self.reset_on_check_ok()
+            self.send_recovery_alert()
+
+    def send_problem_alert(self):
+        self.alert_sent = True
+        self.logger.warning("Sending problem alert to: %s" % ", ".join(self.alert_emails))
+        self.mailer.alert_problem(self.alert_emails, self.checked_host, self.check_command)
+
+    def send_recovery_alert(self):
+        if self.alert_on_recovery:
+            self.logger.info("Sending recovery alert to: %s" % ", ".join(self.alert_emails))
+            self.mailer.alert_recovery(self.alert_emails, self.checked_host, self.check_command)
+
+    def run_shell_command(self, command, capture_output=True):
+        kwargs = {}
+        if capture_output:
+            kwargs.update({
+                'stdout': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'universal_newlines': True,
+            })
+        try:
+            proc = subprocess.Popen(command, **kwargs)
+            stdout, stderr = proc.communicate()
+            returncode = proc.returncode
+        except Exception as e:
+            stdout = ''
+            stderr = e.message if hasattr(e, 'message') else str(e)
+            returncode = 1
+        return returncode, stdout, stderr
+
+    def build_command_args(self):
+        args = [
+            self.nrpe_binary,
+        ]
+        for arg, val in self.config['nrpe'].items():
+            args.append(arg)
+            if val is not True:
+                args.append(val)
+        return args
+
+    def execute_check(self):
+        command = self.build_command_args()
+        self.logger.debug("Running check command: %s" % " ".join(command))
+        returncode, stdout, stderr = self.run_shell_command(command)
+        if returncode == 0:
+            self.logger.debug("Check %s on host %s succeeded" % (self.check_command, self.checked_host))
+            self.handle_recovery()
+            return True
+        self.logger.warning("Check %s on host %s failed, stdout: %s, stderr: %s" % (self.check_command, self.checked_host, stdout, stderr))
+        self.handle_failure()
+        return False
+
+    def configure_next_action(self, result):
+        if self.check_failure_threshold():
+            self.set_sleep_seconds(self.retry_interval)
+        else:
+            self.set_sleep_seconds(self.check_interval)
+        self.logger.debug("Set interval to %d seconds" % self.sleep_seconds)
 
     def monitor(self):
         self.logger.info("Starting poor-mans-nagios with check_interval: %d, retry_interval: %d, failure_threshold: %d" % (self.check_interval, self.retry_interval, self.failure_threshold))
         try:
             while True:
                 result = self.execute_check()
-                self.next_action(result)
+                self.configure_next_action(result)
+                self.logger.debug("Sleeping %d seconds" % self.sleep_seconds)
+                time.sleep(self.sleep_seconds)
         except KeyboardInterrupt:
             self.logger.warning('Process interrupted')
